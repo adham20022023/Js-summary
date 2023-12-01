f6739f354646b1ab9964b6a8a60357b02ec2b106
Adham Elganzoury <adham20022020@Protonmail.com>
2023-12-01 16:42:11 +0200

    third hour

--- main.js
+++
@@ -1,9 +0,0 @@
-let mynum= 123456.798;
-// mynum=mynum.toLocaleString("en-US");
-// mynum=mynum.toLocaleString("hi-IN");
-// mynum=mynum.toLocaleString("en-US", {style:"currency", currency:"USD"});
-let mynum2=0.5;
-mynum2=mynum2.toLocaleString(undefined,{style:"percent"});
-let mynum3=55;
-mynum3=mynum3.toLocaleString(undefined,{style:"unit", unit:"celsius"});
-console.log(mynum3);
